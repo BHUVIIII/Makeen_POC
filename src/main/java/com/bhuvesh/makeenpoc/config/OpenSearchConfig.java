60d3005fbc65325ef36930fba0ee1604b5a200b8
BHUVIIII <bhuveshsingh1969@gmail.com>
2024-11-23 06:42:28 +0530

    Poc Project

---
+++ src/main/java/com/bhuvesh/makeenpoc/config/OpenSearchConfig.java
@@ -0,0 +1,17 @@
+package com.bhuvesh.makeenpoc.config;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenSearchConfig {
+
+	@Bean
+	public RestHighLevelClient openSearchClient() {
+		return new RestHighLevelClient(RestClient.builder(new HttpHost("localhost",9200,"http")));
+	}
+	
+}
